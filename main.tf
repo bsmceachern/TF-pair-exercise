f0b187a0f87b8d205e9d59f43efb866aed961de3
Branden <bsmceachern@gmail.com>
2021-12-15 14:44:44 -0600

    added route table with reference to IGW

--- main.tf
+++ main.tf
@@ -39,3 +39,16 @@ tags = {
   }
 }
 
+#Route Table
+resource "aws_route_table" "b-h-route-table" {
+  vpc_id = aws_vpc.B-H-VPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.B-H-IGW.id
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
